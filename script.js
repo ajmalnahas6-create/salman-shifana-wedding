b5c073cedd1fff7638dbe4e66842d7a71c5d442c
ajmalnahas6-create <ajmalnahas6@gmail.com>
2026-07-12 19:42:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector("button");
 
+    btn.addEventListener("click", () => {
+        alert("✨ Welcome to Salman ❤️ Shifana's Royal Nikah Invitation ✨");
+    });
+});
